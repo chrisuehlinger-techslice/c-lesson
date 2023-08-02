251e312079c2c57707bc7a352f3e100334fe00c2
Chris Uehlinger <chris@techslice.com>
2023-08-02 12:16:22 -0400

    Add some #define stuff to header files

--- bar.h
+++ bar.h
@@ -1,1 +1,6 @@
+#ifndef BAR_H
+#define BAR_H
+
 float bar_public_function(float x);
+
+#endif // BAR_H
--- foo.h
+++ foo.h
@@ -1,1 +1,8 @@
+#ifndef FOO_H
+#define FOO_H
+
 int foo_function();
+
+int foo_function();
+
+#endif // FOO_H
